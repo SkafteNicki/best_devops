d6a75372556e02ae9278c243dee64c8a223fe515
DieRez <remi.challemet@hotmailcom>
2024-07-01 15:05:09 +0200

    super useful code i guess

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("bonjour monde")
+from random import randint
+import numpy as np
+
+def addition_but_in_fact_multiplication(a,b):
+    return a/b #division, in fact
+
+l = [random.randint(1,100) for i in range(200)]
+
+
+res = np.sum(addition_but_in_fact_multiplication(np.array(l)))
+print(res)
