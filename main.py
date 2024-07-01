cc90b49a5295f1ad0a05a6ae7dd52f6c95acc169
Anna <anna11092205@gmail.comm>
2024-07-01 16:52:25 +0300

    fg

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Імпортуємо необхідні бібліотеки
+
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
@@ -6,41 +6,47 @@ from sklearn.svm import SVC
 from sklearn.metrics import classification_report, accuracy_score
 import numpy as np
 
-# Додаємо нову функцію add з гілки new-branch-anyta
 def add(a: int | float | np.ndarray, b: int | float | np.ndarray) -> int | float | np.ndarray:
     return a + b
 
+print("SVM model")
+
 def train_and_evaluate():
-    # Завантажуємо набір даних
+    # TODO: add arguments and argument parsing for high-level configuration
+
+    # Load the dataset
     data = load_breast_cancer()
     X = data.data
     y = data.target
 
-    # Розділяємо набір даних на тренувальну і тестову вибірки
+    # Split the dataset into training and testing sets
+    # TODO: consider using cross-validation
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=8)
 
-    # Стандартизуємо ознаки
+    # Standardize the features
+    # TODO: consider better feature engineering
     scaler = StandardScaler()
     X_train = scaler.fit_transform(X_train)
     X_test = scaler.transform(X_test)
 
-    # Тренуємо модель SVM
+    # Train a Support Vector Machine (SVM) model
+    # TODO: consider using a different model
+    # TODO: consider hyperparameter tuning
     model = SVC(kernel='linear', random_state=8)
     model.fit(X_train, y_train)
 
-    # Робимо передбачення на тестовій вибірці
+    # Make predictions on the test set
     y_pred = model.predict(X_test)
 
-    # Оцінюємо модель
+    # Evaluate the model
+    # TODO: consider using more evaluation metrics
     accuracy = accuracy_score(y_test, y_pred)
     report = classification_report(y_test, y_pred)
-    
-    print("I don`t know")
+
     print(f'Accuracy: {accuracy:.3f}')
     print('Classification Report:')
-    print("Hello")
+    print('Hello')
     print(report)
-    
     return accuracy, report
 
 if __name__ == "__main__":
